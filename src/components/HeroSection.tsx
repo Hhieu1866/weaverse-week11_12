dbe174608cfaac56eebb9e2f34a1b653aa6a233e
HieuK1502 <hoanghieu7a3@gmail.com>
2025-09-16 00:38:31 +0700

    clean up

--- src/components/HeroSection.tsx
+++
@@ -1,8 +0,0 @@
-import FancyButton from "./ui/FancyButton";
-import { AspectRatio } from "./ui/aspect-ratio";
-
-const HeroSection = () => {
-  return <div></div>;
-};
-
-export default HeroSection;
